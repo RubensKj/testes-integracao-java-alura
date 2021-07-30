405ce2a09d0fe43aeef46aca239f4832ced20879
rubenskj <rubenskleinschmidtjr@gmail.com>
2021-07-30 15:53:33 -0300

    adding timeouts

--- src/test/java/br/com/alura/leilao/selenium/page/PageObject.java
+++ src/test/java/br/com/alura/leilao/selenium/page/PageObject.java
@@ -3,16 +3,28 @@ package br.com.alura.leilao.selenium.page;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public abstract class PageObject extends ChromePage {
 
     private final WebDriver webDriver;
 
     public PageObject() {
         this.webDriver = new ChromeDriver();
+
+        addTimeouts();
     }
 
     public PageObject(WebDriver webDriver) {
         this.webDriver = webDriver;
+
+        addTimeouts();
+    }
+
+    private void addTimeouts() {
+        this.webDriver.manage().timeouts()
+                .implicitlyWait(5, TimeUnit.SECONDS)
+                .pageLoadTimeout(10, TimeUnit.SECONDS);
     }
 
     public void quitBrowser() {
